2ae00606e77f2f78fa74165da93d03324c63eaee
kimsjava <kimsjava@gmail.com>
2021-02-19 11:04:37 +0900

    totalbbs-example first commit

---
+++ src/main/java/site/totalbbs/sample/TotalbbsExampleApplication.java
@@ -0,0 +1,13 @@
+package site.totalbbs.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalbbsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalbbsExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/site/totalbbs/sample/configuration/package-info.java
@@ -0,0 +1,1 @@
+package site.totalbbs.sample.configuration;
